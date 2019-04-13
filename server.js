3315a12ba5d246c2f6e909ff8d4b1e57da618871
Valerie S <43964677+vs0018@users.noreply.github.com>
2019-04-13 13:25:58 -0400

    adding Sequelize

--- server.js
+++ server.js
@@ -1,13 +1,43 @@
+require('dotenv').config({ path: 'client/.env.local' });
+
 const express = require("express");
+const cors = require('cors');
+const Sequelize = require('sequelize');
+const epilogue = require('epilogue');
+const OktaJwtVerifier = require('@okta/jwt-verifier');
+
 const routes = require("./routes");
 
+const oktaJwtVerifier = new OktaJwtVerifier({
+  clientId: process.env.REACT_APP_OKTA_CLIENT_ID,
+  issuer: `${process.env.REACT_APP_OKTA_ORG_URL}/oauth2/default`,
+});
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 
 // Define middleware here
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(async (req, res, next) => {
+  try {
+    if (!req.headers.authorization) throw new Error('Authorization header is required');
+
+    const accessToken = req.headers.authorization.trim().split(' ')[1];
+    await oktaJwtVerifier.verifyAccessToken(accessToken);
+    next();
+  } catch (error) {
+    next(error.message);
+  }
+});
+
+
+
+const database = new Sequelize({
+  host: 'localhost',
+  dialect: 'mysql'
+});
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -20,7 +50,10 @@ app.use(routes);
 //Connect to DB
 
 
+const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+database.sync().then(() => {
+  app.listen(PORT, function() {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+  });
 });
