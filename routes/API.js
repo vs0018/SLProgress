b8a33c2a04e8cfefaa1de2657fe07588bfeec9ce
Valerie S <43964677+vs0018@users.noreply.github.com>
2019-04-13 23:14:28 -0400

    fix epilogue module

--- routes/API.js
+++ routes/API.js
@@ -1,12 +1,9 @@
-var db = require('../models');
-const epilogue = require('epilogue');
-
 module.exports = function(app) {
   // Using Epiloque for REST endpoints and controllers
-  epilogue.initialize({ app, sequelize: db.sequelize });
+  epilogue.initialize({ app, sequelize: sequelize });
 
   epilogue.resource({
-    model: db.Post,
+    model: Post,
     endpoints: ['/posts', '/posts/:id'],
   });
 };
