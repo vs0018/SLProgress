489409e233b270672c94d42dca837d1649713506
Valerie S <43964677+vs0018@users.noreply.github.com>
2019-04-25 13:13:39 -0400

    create add goal component and add button to client profile page

--- client/src/components/ClientProfile.js
+++ client/src/components/ClientProfile.js
@@ -3,8 +3,10 @@ import { withAuth } from '@okta/okta-react';
 import {
   withStyles,
   Button,
+  IconButton,
   Typography
 } from '@material-ui/core';
+import { Add as AddIcon } from '@material-ui/icons';
 import { compose } from 'recompose';
 import { withRouter } from 'react-router-dom';
 import API from "../utils/API";
@@ -13,6 +15,15 @@ import AddGoal from './AddGoal'
 const styles = theme => ({
   marginTop: {
     marginTop: 2 * theme.spacing.unit,
+  },
+  fab: {
+    position: 'absolute',
+    bottom: 3 * theme.spacing.unit,
+    right: 3 * theme.spacing.unit,
+    [theme.breakpoints.down('xs')]: {
+      bottom: 2 * theme.spacing.unit,
+      right: 2 * theme.spacing.unit,
+    },
   },
 });
 
@@ -35,6 +46,16 @@ class ClientProfile extends Component {
     return (
     <Fragment>
         <Typography variant="display1">{this.state.client.firstName}'s Page</Typography>
+        <Button
+          variant="fab"
+          color="secondary"
+          aria-label="add"
+          className={classes.fab}
+          component={Link}
+          to="/clients/addgoal"
+        >
+          <AddIcon />
+        </Button>
     </Fragment>
     );
   }
